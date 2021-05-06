aca50e74d3b2c9f756acb917688cc4679fa09a26
Elliot Robertson <elliotrobertson53@gmail.com>
2021-05-06 10:01:18 +0100

    test Pass. Conflicting argument for test 3

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function calculateBill(price, vat, tip) {
-    
+    if (price || tip || vat === !Number)
+    return "error"
+
     const total = (price + (price * vat /100) + tip).toString();
     let currency = "£"
     return currency + total 
